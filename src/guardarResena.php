e4aa5f3f46556467928ef3027843e45abf56a61b
OrtizJackelin <ortizclaudis@gmail.com>
2023-10-22 22:22:35 -0300

    Se crep guardar reseña , es la pagina que resive los datos madados por ajax para guaradar las reseñas en ña bd

---
+++ src/guardarResena.php
@@ -0,0 +1,41 @@
+<?php 
+    require_once ('sessionStart.php'); 
+    
+    if(!isset($_SESSION['id'])){
+        header("Location: index.php");
+        exit;
+        var_dump($_SESSION);
+    }
+    
+    $valido = true;
+    $mensaje = "";    
+            
+    try{
+        include "bd/conexion.php";
+    
+    } catch (mysqli_sql_exception $e) {
+        $mensajeError = "Error en la conexión a la base de datos: " . $e->getMessage();
+        // redirigir al cliente a una página de error personalizada o mostrar un mensaje en la página actual
+        header("Location: error.php?mensaje=" . urlencode($mensajeError));
+            
+    }
+    //var_dump($_POST);
+    extract($_POST);
+    $consulta = "INSERT INTO reseña
+    SET id_usuario = ?, id_publicacion = ?, comentario = ?, calificacion = ?";
+
+    $sentencia = $conexion->stmt_init();
+    if(!$sentencia->prepare($consulta)){
+        echo "Error preparando la consulta para insertar datos ";
+    } else{
+        $sentencia ->bind_param("ssss", $idUsuario,  $idPublicacion, $comentario, $calificacion);
+        $sentencia->execute();
+
+    if($sentencia->affected_rows <= 0) {
+        echo "No se guardo en bd el valor ya existia <br>";     
+    }
+    $sentencia->close();   
+        echo "Solicitud actualizada con éxito en bd verificacion de solicitudes";
+    }  
+    include "bd/cerrar_conexion.php";      
+?>
